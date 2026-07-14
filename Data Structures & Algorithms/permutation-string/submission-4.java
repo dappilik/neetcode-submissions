fb5164343676931f7f84c468b7d0fafa99d7abcd
Krishna Reddy <dappili.k@live.com>
2026-07-14 05:58:29 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] f1 = new int[26];
+        int[] f2 = new int[26];
+        int left = 0;
+
+        for (char c : s1.toCharArray()) {
+            f1[c - 'a']++;
+        }
+
+        for (int right = 0; right < s2.length(); right++) {
+            f2[s2.charAt(right) - 'a']++;
+            if (right - left + 1 < s1.length()) {
+                continue;
+            } else if (right - left + 1 > s1.length()) {
+                f2[s2.charAt(left) - 'a']--;
+                left++;
+            }
+            System.out.println(left + ", " + right);
+
+            System.out.println(Arrays.toString(f1));
+            System.out.println(Arrays.toString(f2));
+
+            if (Arrays.equals(f1, f2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
